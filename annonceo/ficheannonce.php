c24c6dbffc06d1f94227c7c62a2717b93fd9f8c8
corinne1976 <corinne.tina@lepoles.com>
2017-10-19 16:47:13 +0200

    commit11
    fiche annonce

---
+++ annonceo/ficheannonce.php
@@ -0,0 +1,104 @@
+<?php require('inc/init.inc.php'); ?>
+<?php require('inc/header.inc.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Appartement</h1>
+                <div class=pull-right>
+                    <button type="button" class="btn btn-success">Contacter Marie</button>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <hr>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="img/photo2.jpg"  width="100%" alt="#">
+                    </div>
+                    <div class="col-md-6">
+                        <span >Description</span><br>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                    </div>
+
+                </div>
+                <div class="col-md-3"><br>
+                    <p><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
+
+                        Date de publication : 15-05-2017</p>
+                    </div>
+                    <div class="col-md-3"><br>
+                        <p><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Marie <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span><span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span><span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span><span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span><span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span></p>
+                    </div>
+                    <div class="col-md-3"><br>
+                        <p><span class="glyphicon glyphicon-euro" aria-hidden="true"></span> 1000 €</p>
+                    </div>
+                    <div class="col-md-3"><br>
+                        <p><span class="glyphicon glyphicon-tags" aria-hidden="true"></span>  2 rue lamotte picquet 44000 nantes</p>
+                    </div>
+                    <div class="col-md-12">
+                        <iframe width="100%" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2710.207049454749!2d-1.5729033!3d47.2125309!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4805ec0fcda6c4cb%3A0xd620ca38dafa1e9a!2s2+Rue+La+Motte+Picquet%2C+44100+Nantes!5e0!3m2!1sfr!2sfr!4v1423244007186" width="600" height="450" frameborder="0"></iframe>
+
+                    </div>
+                    <div class="col-md-12">
+                        <p>autres annonces</p>
+                        <hr>
+                    </div>
+                    <div class="col-md-3">
+                        <img src="img/photo3.jpg" alt="photo">
+                    </div>
+                    <div class="col-md-3">
+                        <img src="img/photo4.jpg" alt="photo">
+                    </div>
+                    <div class="col-md-3">
+                        <img src="img/photo5.jpg" alt="photo">
+                    </div>
+                    <div class="col-md-3">
+                        <img src="img/photo6.jpg" alt="photo">
+                    </div>
+                    <hr>
+                    <div class="col-md-12">
+                        <a href="#">déposer un commentaire ou une note</a>
+                        <div class=pull-right>
+
+                            <a href="#">retour vers les annonces</a>
+                        </div>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php require('inc/footer.inc.php');?>
